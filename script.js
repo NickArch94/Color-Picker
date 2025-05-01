98f6b176b002b7562f49fc7edb9ee34602700031
NickArch94 <158612778+NickArch94@users.noreply.github.com>
2025-04-30 19:38:11 -0600

    Finished out the 'Script.js' file as demonstrated in the video, added some custom attributes to the 'style.css' file, and tightened up the HTML.

---
+++ script.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+    initColorPicker();
+
+};
+
+function initColorPicker() {
+    let colorBox = document.getElementById('color-box');
+    let rgb = {
+        red: document.getElementById("red"),
+        green: document.getElementById("green"),
+        blue: document.getElementById("blue"),
+    };
+    let colorPickers = document.getElementsByClassName('picker');
+    setColorPickerEventListeners(colorBox, rgb, colorPickers);
+}
+
+function setColorPickerEventListeners(element, colors, pickerElements) {
+    let pickerLen = pickerElements.length;
+    for (let i= 0; i < pickerLen; i++) {
+        pickerElements[i].addEventListener('change', () => {
+            setElementBackgroundColor(element, colors.red.value, colors.green.value, colors.blue.value);
+            setDisplayValues(colors.red.value, colors.green.value, colors.blue.value);
+        } );
+    }
+
+function setElementBackgroundColor(bgElement, red, green, blue) {
+    let rgbVal = [red, green, blue].join(',');
+    bgElement.style.backgroundColor = 'rgb(' + rgbVal + ')';
+}}
+
+function setDisplayValues(red, green, blue) {
+    let redVal = document.getElementById("redVal");
+    let greenVal = document.getElementById("greenVal");
+    let blueVal = document.getElementById("blueVal");
+
+    redVal.innerText = red;
+    greenVal.innerText = green;
+    blueVal.innerText = blue;
+}
